6ee52e374a8fd53a5ce90bf03d3ed9debb971b05
zhuth <zhu@tianhua.me>
2019-04-16 08:03:55 +0800

    ui fix

--- nodes.py
+++ nodes.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import requests, subprocess, json, os, sys, base64, time
+import requests
+import subprocess
+import json
+import os
+import sys
+import base64
+import time
 from flask import Response
 import re
 
 
 def _curl(url, timeout=None):
-    if timeout is None: timeout = StatusNode.timeout
+    if timeout is None:
+        timeout = StatusNode.timeout
     try:
         return requests.get(url, timeout=timeout)
     except requests.exceptions.ReadTimeout:
@@ -32,7 +39,7 @@ class StatusNode:
 
     def detect_power(self):
         return os.system('/bin/ping -c 1 {} > /dev/null'.format(self.ip)) == 0
-        
+
     def jcurl(self, *args):
         return json.loads(_curl('http://{}:10000/node/self{}'.format(self.ip, ('/' + '/'.join(args)) if len(args) else '')).content.decode('utf-8'))['resp']
 
@@ -49,7 +56,7 @@ class StatusNode:
                 print('Error while loading from {}'.format(self.ip))
                 pass
         return self._services if isinstance(self._services, list) else []
-        
+
     def _get_status(self):
         try:
             return self.jcurl()
@@ -69,15 +76,17 @@ class StatusNode:
 
     def set_buffer(self, request_json, **kwargs):
         self._status_buf = request_json['status'] or {}
-        if not isinstance(self._services, list): self._services = request_json.get('services', [])
+        if not isinstance(self._services, list):
+            self._services = request_json.get('services', [])
         self._status_buf['power'] = True
         self.last_update = time.time()
         self._status_buf['last_update'] = self.last_update
-    
+
     def _power(self, cmd):
         if self.power_ip:
             assert cmd in ['on', 'off', 'uflash', 'flash']
-            assert _curl('http://{}/{}'.format(self.power_ip, cmd)).status_code == 200
+            assert _curl('http://{}/{}'.format(self.power_ip, cmd)
+                         ).status_code == 200
             return True
 
     def force_off(self):
@@ -91,9 +100,10 @@ class StatusNode:
         if self.power_ip and self.power_ip.startswith("wol:"):
             wolmac = self.power_ip[4:].replace(':', '-').strip()
             from wakeonlan import send_magic_packet
-            send_magic_packet(wolmac, ip_address='.'.join(self.ip.split('.')[:3]+['255']))
+            send_magic_packet(wolmac, ip_address='.'.join(
+                self.ip.split('.')[:3]+['255']))
             return True
-        elif self.ip and self.power_ip: # computer with wifi power button
+        elif self.ip and self.power_ip:  # computer with wifi power button
             return self._power('uflash')
 
     def power_off(self):
@@ -114,13 +124,15 @@ class StatusNode:
 
     def node(self, node_name, *other_params):
         op = '/'.join(other_params)
-        if op: op = '/' + op
-        resp = _curl('http://{}:10000/node/{}{}'.format(self.ip, node_name, op), timeout=2)
+        if op:
+            op = '/' + op
+        resp = _curl('http://{}:10000/node/{}{}'.format(self.ip,
+                                                        node_name, op), timeout=2)
         if resp.headers['content-type'] == 'application/json':
             return json.loads(resp.content.decode('utf-8'))
         else:
             return Response(resp.content, content_type=resp.headers['content-type'])
-        
+
     def refresh_status(self):
         self.last_update = 0
 
@@ -138,9 +150,10 @@ class AirPurifier(StatusNode):
 
     def detect_power(self):
         return None
-        
+
     def icon_base64(self, aqio):
-        if not isinstance(aqio, str): aqio = str(int(aqio))
+        if not isinstance(aqio, str):
+            aqio = str(int(aqio))
         aqi_icon = self.icon(aqio)
         return 'data:image/png;base64,' + base64.b64encode(aqi_icon).decode('ascii')
 
@@ -153,13 +166,16 @@ class AirPurifier(StatusNode):
             skt.sendall(cmd.encode('utf-8'))
             if cmd == '?':
                 tr = skt.recv(1024)
-                r = json.loads(tr[tr.find(b'{'):tr.rfind(b'}')+1].decode('utf-8'))
-                if r['aqi'] > 1000: raise Exception("Data range error.")
+                r = json.loads(
+                    tr[tr.find(b'{'):tr.rfind(b'}')+1].decode('utf-8'))
+                if r['aqi'] > 1000:
+                    raise Exception("Data range error.")
             else:
                 r = b''
                 while True:
                     tr = skt.recv(1024)
-                    if not tr: break
+                    if not tr:
+                        break
                     r += tr
             skt.close()
         except TypeError as e:
@@ -187,17 +203,19 @@ class AirPurifier(StatusNode):
         }
 
     def city_aqi(self):
-        js = requests.get('http://feed.aqicn.org/feed/{}/en/feed.v1.js'.format(self.city)).content.decode('utf-8')
+        js = requests.get(
+            'http://feed.aqicn.org/feed/{}/en/feed.v1.js'.format(self.city)).content.decode('utf-8')
         m = re.search(r'>(\d+)<', js)
         if m:
             return m.group(1)
         else:
             return '-'
-        
+
     def _get_status(self):
         d = self.call_command('?')
         pred = self.aqi_pred()
-        d['aqi_pred'] = self.icon_base64('{}:{}'.format(int(pred['first_half']*30), int(pred['second_half']*30)))
+        d['aqi_pred'] = self.icon_base64('{}:{}'.format(
+            int(pred['first_half']*30), int(pred['second_half']*30)))
         d['city_aqi'] = self.icon_base64(self.city_aqi())
         return d
 
@@ -238,25 +256,26 @@ class SwitchNode(StatusNode):
     """
     Power Node
     """
+
     def __init__(self, power_ip):
         super().__init__(self, power_ip=power_ip, services=None)
-    
-    def detect_power(self):        
+
+    def detect_power(self):
         try:
             return _curl('http://{}/'.format(self.power_ip)).content == b'ON'
         except TimeoutError:
             pass
-            
+
     def _get_status(self):
         return {}
 
     def set_service(self, service_name, cmd):
         pass
-        
+
     def power_off(self):
         self._power('off')
         return True
-    
+
     def power_on(self):
         self._power('on')
         return True
@@ -266,54 +285,53 @@ class KonkeNode(SwitchNode):
     """
     Konke Switch
     """
+
     def __init__(self, power_ip):
-        super(SwitchNode, self).__init__(self, power_ip)
+        super().__init__(power_ip)
         from pykonkeio import Switch
         self._konke = Switch(self.power_ip)
 
     def detect_power(self):
         return self._konke.status == 'open'
-        
+
     def power_off(self):
         self._konke.turn_off()
         return True
-    
+
     def power_on(self):
         self._konke.turn_on()
         return True
 
 
 class DelegateNode(StatusNode):
-    
+
     def __init__(self, name, parent, services=None, interval=0):
         super().__init__(services=services, interval=interval)
         self.parent = parent
         self.name = name
-        
+
     @staticmethod
     def resp(r):
-        if isinstance(r, dict): return r.get('resp')
+        if isinstance(r, dict):
+            return r.get('resp')
         return r
-        
+
     def _get_status(self):
         return DelegateNode.resp(self.parent.node(self.name, 'get_status'))
-        
+
     def detect_power(self):
         return DelegateNode.resp(self.parent.node(self.name, 'detect_power'))
-        
+
     def power_on(self):
         return DelegateNode.resp(self.parent.node(self.name, 'power_on'))
-    
+
     def power_off(self):
         return DelegateNode.resp(self.parent.node(self.name, 'power_off'))
-        
+
     def set_service(self, service_name, cmd):
         r = DelegateNode.resp(self.parent.node(self.name, 'set_service'))
         return r
-        
-    def get_status(self):
-        return super().get_status()
-        
+
     def __getattr__(self, name):
         def deal(*args):
             try:
@@ -321,16 +339,17 @@ class DelegateNode(StatusNode):
             except TimeoutError:
                 pass
         return deal
-    
+
     @property
     def services(self):
         if self._services == "auto":
-            self._services = DelegateNode.resp(self.parent.node(self.name, 'services'))
+            self._services = DelegateNode.resp(
+                self.parent.node(self.name, 'services'))
         return self._services
 
 
 class TpLinkRouterNode(StatusNode):
-    
+
     def __init__(self, ip, password, interval=0):
         super().__init__(ip=ip, interval=interval)
         from tplink_api.tplink import TpLinkRouter
@@ -340,22 +359,28 @@ class TpLinkRouterNode(StatusNode):
             {'name': 'wlan2g', 'dispname': '2.4GHz'},
             {'name': 'wlan5g', 'dispname': '5GHz'},
         ]
-        
+
     def _get_status(self):
         self.router.login(self.password)
         wl = self.router.get_wireless()
-        return { # "wl":wl,
+        return {  # "wl":wl,
             'services': {
-                'wlan2g': wl['wireless']['wlan_host_2g']['enable'] == "1",
-                'wlan5g': wl['wireless']['wlan_host_5g']['enable'] == "1"
+                'wlan2g':
+                    {'name': 'wlan2g', 'dispname': '2.4GHz',
+                        'status': wl['wireless']['wlan_host_2g']['enable'] == "1"},
+                'wlan5g':
+                    {'name': 'wlan5g', 'dispname': '5GHz',
+                        'status': wl['wireless']['wlan_host_5g']['enable'] == "1"}
             }
         }
 
     def reboot(self):
         return self.router.reboot()
-    
+
     def set_service(self, service_name, cmd):
         self.router.login(self.password)
         if service_name in ('wlan2g', 'wlan5g'):
-            if cmd == 'restart': self.router.reboot()
-            else: self.router.set_wireless(cmd == 'start', service_name[-2:])
+            if cmd == 'restart':
+                self.router.reboot()
+            else:
+                self.router.set_wireless(cmd == 'start', service_name[-2:])
--- static/controls.js
+++ static/controls.js
@@ -1,45 +1,46 @@
 (function() {
-	
-	const $ = jQuery, $get = $.get;
-	
-	Vue.component('button-toggler', {
-		props: ['node', 'val', 'action', 'service'],
-		data: function () {
-			return {
-				disabled: false
-			}
-		},
-		template: `
+
+    const $ = jQuery,
+        $get = $.get;
+
+    Vue.component('button-toggler', {
+        props: ['node', 'val', 'action', 'service'],
+        data: function() {
+            return {
+                disabled: false
+            }
+        },
+        template: `
 		<button @click="toggle" class="mui-btn mui-btn--fab"
 				:class="{ 'button-on': val === true, 'button-off': val === false }"
 				:disabled="disabled"
 				v-show="typeof(val) === 'boolean'">{{ val ? '开' : '关' }}
 		</button>`,
-		methods: {
-			toggle () {
-				var action = this.action;
-				this.disabled = true;
-				if (action === 'power') action += this.val ? '_off' : '_on';
-				else if (action.substr(0, 7) === 'service') action = 'set_' + action + '/' + (this.val ? 'stop' : 'start');
-				$get('node/' + this.node + '/' + action).then((data) => {
-						if (data.error) alert(data.error);
-					}).then((error) => {
-						this.disabled = false;
-					});
-				this.$emit('click');
-			}
-		}
-	});
-	
-	Vue.component('temperature-viewer', {
-		props: ['val'],
-		computed: {
-			val_deg() {
-				return (this.val || "").replace(/(\d+(\.\d+)?)d/g, "$1°C");
-			}
-		},
-		template: `<span>{{ val_deg }}</span>`
-	});	
+        methods: {
+            toggle() {
+                var action = this.action;
+                this.disabled = true;
+                if (action === 'power') action += this.val ? '_off' : '_on';
+                else if (action.substr(0, 7) === 'service') action = 'set_' + action + '/' + (this.val ? 'stop' : 'start');
+                $get('node/' + this.node + '/' + action).then((data) => {
+                    if (data.error) alert(data.error);
+                }).then((error) => {
+                    this.disabled = false;
+                });
+                this.$emit('click');
+            }
+        }
+    });
+
+    Vue.component('temperature-viewer', {
+        props: ['val'],
+        computed: {
+            val_deg() {
+                return (this.val || "").replace(/(\d+(\.\d+)?)d/g, "$1°C");
+            }
+        },
+        template: `<span>{{ val_deg }}</span>`
+    });
 
     var app = new Vue({
         el: '#app',
@@ -47,124 +48,108 @@
             config: config,
             nodes: [],
             switch_nodes: [],
-            services: [],
             resp: {},
-            services: [],
             current: [''],
             output: '',
-			_t: 0
+            _t: 0
         },
         methods: {
-            navigate (u) {
+            navigate(u) {
                 location.hash = '#' + u;
             },
-            load_all_services () {
-                this.services = [];
-                $get('node/self/all_services').then((data) => {
-                    this.services = data.resp;
-                });
+            action_button(node, action, param) {
+                switch (action) {
+                    case 'link':
+                        location.href = param.link;
+                        return;
+                    default:
+                        break;
+                }
+
+                if (action) {
+                    $get('node/' + node + '/' + action)
+                        .then((data) => {
+                            if (data.error) alert(data.error);
+                            if (data.resp && data.resp) this.output = data.resp;
+                        })
+                        .then(() => {
+                            this.disabled = false;
+                            update();
+                        });
+                }
             },
-			action_button (node, action, param) {
-				switch (action) {
-					case 'link':
-						location.href = param.link;
-						return;
-					default:
-						break;
-				}
+            update() {
+                if (this.current[0] === 'cameras') {
+                    this._t = Math.random();
+                } else {
+                    var nodes = this.nodes;
+                    if (this.current[0] === 'switches') nodes = this.switch_nodes;
+                    else if (this.current[0] === 'node') nodes = [this.current[1]];
 
-				if (action) {
-					$get('node/' + node + '/' + action)
-						.then((data) => {
-							if (data.error) alert(data.error);
-							if (data.resp && data.resp) this.output = data.resp;
-						})
-						.then(() => {
-							this.disabled = false;
-							update();
-						});
-				}
-			},
-			update () {
-				if (this.current[0] === 'cameras') {
-					this._t = Math.random();
-				} else {
-					var nodes = this.nodes;
-					if (this.current[0] === 'switches') nodes = this.switch_nodes;
-					else if (this.current[0] === 'node') nodes = [this.current[1]];
-
-					for (let n of nodes) {
-						$get('node/' + n).then((data) => {
-							if (data.resp) this.resp[n] = data.resp;
-							this.$forceUpdate();
-						});
-					}
-				}
-				this.$forceUpdate();
-			}
+                    for (let n of nodes) {
+                        $get('node/' + n).then((data) => {
+                            if (data.resp) this.resp[n] = data.resp;
+                            this.$forceUpdate();
+                        });
+                    }
+                }
+                this.$forceUpdate();
+            }
         }
     });
-	
-	const
-		$bodyEl = $('body'),
-		$sidedrawer = $('#sidedrawer');
 
-	$get('node/self').done((data) => {
-		app.nodes.push('self');
-		for (let node in data.resp.nodes) {
-			if (['SwitchNode', 'KonkeNode'].indexOf(data.resp.nodes[node]) >= 0) {
-				app.switch_nodes.push(node);
-			} else {
-				app.nodes.push(node);
-			}
-		}
-	});
+    $.get('node/self').then((data) => {
+        for (let node in data.resp.nodes) {
+            if (['SwitchNode', 'KonkeNode'].indexOf(data.resp.nodes[node]) < 0) app.nodes.push(node);
+            else app.switch_nodes.push(node);
+        }
+    });
 
-	$(window).on('hashchange', function(e) {
-		const
-			$this = $(this),
-			hash = location.hash.substr(1).split('/');
+    $(window).on('hashchange', function(e) {
+        const
+            hash = location.hash.substr(1).split('/');
 
-		app.current = hash;
-		app.services = [];
-		app.output = '';
-		if (hash[0] == 'services') app.load_all_services();
-		app.update();
-	});
+        app.current = hash;
+        app.output = '';
+        app.update();
+    });
 
-	$('section').hide();
-	if (location.hash === '') location.hash = 'node/' + app.config.default_node;
-	else $(window).trigger('hashchange');
+    if (location.hash === '') location.hash = '#' + config.default_hash;
+    else $(window).trigger('hashchange');
 
-	setInterval(app.update, 5000);
+    setInterval(app.update, 5000);
 
+    (function() { // side drawer
 
-	(function() { // side drawer
-		$('.js-show-sidedrawer').on('click', function() {
-			var options = {
-				onclose: function() {
-					$sidedrawer
-						.removeClass('active')
-						.appendTo(document.body);
-				}
-			};
+        const
+            $bodyEl = $('body'),
+            $sidedrawer = $('#sidedrawer');
 
-			var $overlayEl = $(mui.overlay('on', options));
-			$sidedrawer.appendTo($overlayEl);
-			setTimeout(function() {
-				$sidedrawer.addClass('active');
-			}, 20);
-		});
+        $('.js-show-sidedrawer').on('click', function() {
+            var options = {
+                onclose: function() {
+                    $sidedrawer
+                        .removeClass('active')
+                        .appendTo(document.body);
+                }
+            };
 
-		$('.js-hide-sidedrawer').on('click', function() {
-			$bodyEl.toggleClass('hide-sidedrawer');
-		});
+            var $overlayEl = $(mui.overlay('on', options));
+            $sidedrawer.appendTo($overlayEl);
+            setTimeout(function() {
+                $sidedrawer.addClass('active');
+            }, 20);
+        });
 
-		$('strong', $sidedrawer).on('click', function() {
-			$(this).next().slideToggle(200);
-		}).next().hide();
+        $('.js-hide-sidedrawer').on('click', function() {
+            $bodyEl.toggleClass('hide-sidedrawer');
+        });
 
-		$('strong', $sidedrawer).first().click();
-	})();
+        $('strong', $sidedrawer).on('click', function() {
+            $(this).next().slideToggle(200);
+        }).next().hide();
+
+        $('strong', $sidedrawer).first().click();
+    })();
 
 })();
